c2f56be56d45d3b91d65a10d80d925e9c13750b2
Sad0328 <tours@soledad.com.ua>
2023-11-23 12:14:18 +0200

    09-23-11

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+
+ let colorInterval;
+
+
+function startChangingColor() {
+startBtn.disabled = true; 
+colorInterval = setInterval(() => {
+document.body.style.backgroundColor = getRandomHexColor();
+}, 1000);
+}
+
+function stopChangingColor() {
+startBtn.disabled = false; 
+clearInterval(colorInterval);
+}
+
+startBtn.addEventListener('click', startChangingColor);
+stopBtn.addEventListener('click', stopChangingColor);
+
+
+
+
